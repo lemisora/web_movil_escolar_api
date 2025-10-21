63c1f8cc0a01f2c95cd48ae7c2987fe1178cacb7
lemisora <leonardo.michel.dom@gmail.com>
2025-10-20 19:19:51 -0600

    Se descentraliza el registro añadiendo más endpoints

--- web_movil_escolar_api/urls.py
+++ web_movil_escolar_api/urls.py
@@ -11,12 +11,17 @@ from web_movil_escolar_api.views import auth
 
 urlpatterns = [
     # Create Admin
-    path("register/", users.UserRegistrationView.as_view()),
-    # Admin Data
-    path("lista-admins/", users.UserProfileView.as_view()),
+    # Endpoints de registro por rol
+    path("register/admin/", users.AdminRegistrationView.as_view()),
+    path("register/profesor/", users.ProfesorRegistrationView.as_view()),
+    path("register/alumno/", users.AlumnoRegistrationView.as_view()),
+    # Profile Data
+    path("profile/admin/", users.AdminProfileView.as_view()),
+    path("profile/profesor/", users.ProfesorProfileView.as_view()),
+    path("profile/alumno/", users.AlumnoProfileView.as_view()),
     # Edit Admin
     # path('admins-edit/', users.AdminsViewEdit.as_view())
-   ]
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
