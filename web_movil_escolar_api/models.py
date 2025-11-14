820ceb58e0f58a09ce020541acfc6cd4773b29fa
lemisora <leonardo.michel.dom@gmail.com>
2025-11-14 15:50:21 -0600

    Formato y corrección de mensaje para alumnos

--- web_movil_escolar_api/models.py
+++ web_movil_escolar_api/models.py
@@ -1,13 +1,8 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from rest_framework.authentication import TokenAuthentication
-from django.contrib.auth.models import AbstractUser, User
-from django.conf import settings
-
-from django.db import models
-from django.contrib.auth.models import User
-
 from rest_framework.authentication import TokenAuthentication
 
 
@@ -60,8 +55,6 @@ class Profesores(models.Model):
             + self.user.last_name
         )
 
-
-# TODO: Agregar perfiles para estudiantes
 class Alumnos(models.Model):
     id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(
@@ -78,7 +71,7 @@ class Alumnos(models.Model):
 
     def __str__(self):
         return (
-            "Perfil del profesor "
+            "Perfil del alumno "
             + self.user.first_name
             + " "
             + self.user.last_name
